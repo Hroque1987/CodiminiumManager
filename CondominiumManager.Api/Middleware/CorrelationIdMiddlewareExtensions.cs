8be3e82d9e27572c237c8ab7aebaf987f693bd60
helder.roque <h.roque1987@gmail.com>
2026-04-23 23:24:14 +0100

    feat: implementation of correlation id.

---
+++ CondominiumManager.Api/Middleware/CorrelationIdMiddlewareExtensions.cs
@@ -0,0 +1,10 @@
+﻿namespace CondominiumManager.Api.Middleware;
+
+public static class CorrelationIdMiddlewareExtensions
+{
+    public static IApplicationBuilder UseCorrelation(this IApplicationBuilder builder)
+    {
+        builder.UseMiddleware<CorrelationIdMiddleware>();
+        return builder;
+    }
+}
