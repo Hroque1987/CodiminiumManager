1e68e46d994fa7ecabd9ee08fa132b59348c655e
helder.roque <h.roque1987@gmail.com>
2026-04-23 20:36:11 +0100

    feat: setup serilog

--- CondominiumManager.Api/Program.cs
+++ CondominiumManager.Api/Program.cs
@@ -3,15 +3,26 @@ using CondominiumManager.Finance;
 using CondominiumManager.Identity;
 using CondominiumManager.Notifications;
 using FastEndpoints;
+using Scalar.AspNetCore;
+using Serilog;
+
+var logger = Log.Logger = new LoggerConfiguration()
+    .Enrich.FromLogContext()
+    .WriteTo.Console()
+    .CreateLogger();
+
+logger.Information("Starting Web Server");
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Host.UseSerilog((_, config) => config.ReadFrom.Configuration(builder.Configuration));
+
 builder.Services.AddFastEndpoints();
 
-builder.Services.AddCondominium(builder.Configuration);
-builder.Services.AddFinance(builder.Configuration);
-builder.Services.AddIdentity(builder.Configuration);
-builder.Services.AddNotifications(builder.Configuration);
+builder.Services.AddCondominium(builder.Configuration, logger);
+builder.Services.AddFinance(builder.Configuration, logger);
+builder.Services.AddIdentity(builder.Configuration, logger);
+builder.Services.AddNotifications(builder.Configuration, logger);
 
 builder.Services.AddOpenApi();
 
@@ -22,6 +33,7 @@ app.UseFastEndpoints();
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
+    app.MapScalarApiReference();
 }
 
 app.UseHttpsRedirection();
--- CondominiumManager.Condominium/CondominiumDependencyInjection.cs
+++ CondominiumManager.Condominium/CondominiumDependencyInjection.cs
@@ -2,15 +2,18 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Serilog;
 
 namespace CondominiumManager.Condominium;
 
 public static class CondominiumDependencyInjection
 {
-    public static IServiceCollection AddCondominium(this IServiceCollection services, ConfigurationManager configuration)
+    public static IServiceCollection AddCondominium(this IServiceCollection services, ConfigurationManager configuration, ILogger logger)
     {
         string? connectionString = configuration.GetConnectionString("CondominiumDb");
         services.AddDbContext<CondominiumDbContext>(options => options.UseSqlServer(connectionString));
+
+        logger.Information("{Module} module services registered", "Condominium");
 
         return services;
     }
